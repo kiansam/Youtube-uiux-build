3f8a3f4e80d29f90df02ea4c0f747321eb5e89c2
Kian Sam <itskiansam@gmail.com>
2022-01-26 11:58:39 +0300

    Header.js

--- src/containers/header/Header.jsx
+++ src/containers/header/Header.jsx
@@ -4,7 +4,11 @@ import "./header.css";
 const Header = () => {
   return (
     <div className="gpt3__header section__padding" id="home">
-      <div className="gpt3__header-content"></div>
+      <div className="gpt3__header-content">
+        <h1 className="gradient__text">
+          Let's Build Something amazing with GPT-3 OpenAI
+        </h1>
+      </div>
     </div>
   );
 };
